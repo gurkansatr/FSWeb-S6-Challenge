85f4fff8fc45e9366100e7ddd9ed5e8c4bdf30b6
gurkansatr <gurkan-satir@hotmail.com>
2023-06-05 01:21:47 +0300

    s6challengecevaplar

--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,16 @@
 // Karakter bileşeniniz buraya gelecek
+
+const Karakter = (props) => {
+    const { item } = props;
+
+    return (
+        <div>
+            {item.name}
+        </div>
+     
+    )
+}
+
+
+
+export default Karakter;
